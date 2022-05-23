aa3ca085020dc8c642a73ecf032e8bfb1493f8be
hunghh.dev <hunghh.dev@gmail.com>
2022-05-23 16:38:18 +0700

    Adding Order Entity implementation methods

---
+++ order-service/order-domain/order-domain-core/src/main/java/dev/hunghh/ordering/system/order/service/domain/exception/OrderDomainException.java
@@ -0,0 +1,13 @@
+package dev.hunghh.ordering.system.order.service.domain.exception;
+
+import dev.hunghh.ordering.system.domain.exception.DomainException;
+
+public class OrderDomainException extends DomainException {
+    public OrderDomainException(String message) {
+        super(message);
+    }
+
+    public OrderDomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
