81916ddaf380ae451d6a0a1d288fe4ba7f74eab7
hunghh.dev <hunghh.dev@gmail.com>
2022-05-24 14:00:34 +0700

    Implemeting Domain Events in Order Service domain layer

---
+++ order-service/order-domain/order-domain-core/src/main/java/dev/hunghh/ordering/system/order/service/domain/OrderDomainService.java
@@ -0,0 +1,17 @@
+package dev.hunghh.ordering.system.order.service.domain;
+
+import dev.hunghh.ordering.system.order.service.domain.entity.Order;
+import dev.hunghh.ordering.system.order.service.domain.entity.Restaurant;
+import dev.hunghh.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import dev.hunghh.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import dev.hunghh.ordering.system.order.service.domain.event.OrderPaidEvent;
+
+import java.util.List;
+
+public interface OrderDomainService {
+    OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant);
+    OrderPaidEvent payOrder(Order order);
+    void approveOrder(Order order);
+    OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages);
+    void cancelOrder(Order order, List<String> failureMessages);
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/dev/hunghh/ordering/system/order/service/domain/event/OrderCancelledEvent.java
@@ -0,0 +1,11 @@
+package dev.hunghh.ordering.system.order.service.domain.event;
+
+import dev.hunghh.ordering.system.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCancelledEvent extends OrderEvent {
+    public OrderCancelledEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
