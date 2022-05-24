a0b6e206002f71e2c0e9739f3f419823c83e8189
hunghh.dev <hunghh.dev@gmail.com>
2022-05-24 10:51:22 +0700

    Adding state changing methods to Order Entity

---
+++ order-service/order-domain/order-domain-core/src/main/java/dev/hunghh/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package dev.hunghh.ordering.system.order.service.domain.entity;
+
+import dev.hunghh.ordering.system.domain.entity.AggregateRoot;
+import dev.hunghh.ordering.system.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
