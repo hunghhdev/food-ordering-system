e45533953103dc6272dbc841dc92a466288b22d8
hunghh.dev <hunghh.dev@gmail.com>
2022-05-24 15:34:18 +0700

    Implementing Order Application Service - DTO classes

---
+++ order-service/order-domain/order-application-service/src/main/java/dev/hunghh/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
@@ -0,0 +1,21 @@
+package dev.hunghh.ordering.system.order.service.domain.dto.create;
+
+import dev.hunghh.ordering.system.domain.valueobject.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderResponse {
+    @NotNull
+    private final UUID orderTrackingId;
+    @NotNull
+    private final OrderStatus orderStatus;
+    @NotNull
+    private final String message;
+}
