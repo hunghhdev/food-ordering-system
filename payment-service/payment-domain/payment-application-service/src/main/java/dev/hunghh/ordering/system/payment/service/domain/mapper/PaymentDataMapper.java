92ef75d245fdc139337289142cfa2ce2072c3b4c
hunghh.dev <hunghh.dev@gmail.com>
2022-06-05 15:12:47 +0700

    Application Service domain module Implementing input ports

--- payment-service/payment-domain/payment-application-service/src/main/java/dev/hunghh/ordering/system/payment/service/domain/mapper/PaymentDataMapper.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/dev/hunghh/ordering/system/payment/service/domain/mapper/PaymentDataMapper.java
@@ -1,7 +1,21 @@
 package dev.hunghh.ordering.system.payment.service.domain.mapper;
 
+import dev.hunghh.ordering.system.domain.valueobject.CustomerId;
+import dev.hunghh.ordering.system.domain.valueobject.Money;
+import dev.hunghh.ordering.system.domain.valueobject.OrderId;
+import dev.hunghh.ordering.system.payment.service.domain.dto.PaymentRequest;
+import dev.hunghh.ordering.system.payment.service.domain.entity.Payment;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class PaymentDataMapper {
+    public Payment paymentRequestModelToPayment(PaymentRequest paymentRequest) {
+        return Payment.builder()
+                .orderId(new OrderId(UUID.fromString(paymentRequest.getId())))
+                .customerId(new CustomerId(UUID.fromString(paymentRequest.getId())))
+                .price(new Money(paymentRequest.getPrice()))
+                .build();
+    }
 }
