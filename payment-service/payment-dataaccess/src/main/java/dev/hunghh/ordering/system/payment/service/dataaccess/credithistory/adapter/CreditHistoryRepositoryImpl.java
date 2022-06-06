3d9bcb9914d69bf67b752a24399755d528b84cd6
hunghh.dev <hunghh.dev@gmail.com>
2022-06-06 07:58:05 +0700

    Update CreditHistoryRepositoryImpl.java

--- payment-service/payment-dataaccess/src/main/java/dev/hunghh/ordering/system/payment/service/dataaccess/credithistory/adapter/CreditHistoryRepositoryImpl.java
+++ payment-service/payment-dataaccess/src/main/java/dev/hunghh/ordering/system/payment/service/dataaccess/credithistory/adapter/CreditHistoryRepositoryImpl.java
@@ -4,9 +4,7 @@ import dev.hunghh.ordering.system.domain.valueobject.CustomerId;
 import dev.hunghh.ordering.system.payment.service.dataaccess.credithistory.entity.CreditHistoryEntity;
 import dev.hunghh.ordering.system.payment.service.dataaccess.credithistory.mapper.CreditHistoryDataAccessMapper;
 import dev.hunghh.ordering.system.payment.service.dataaccess.credithistory.repository.CreditHistoryJpaRepository;
-import dev.hunghh.ordering.system.payment.service.domain.entity.CreditEntry;
 import dev.hunghh.ordering.system.payment.service.domain.entity.CreditHistory;
-import dev.hunghh.ordering.system.payment.service.domain.ports.output.repository.CreditEntryRepository;
 import dev.hunghh.ordering.system.payment.service.domain.ports.output.repository.CreditHistoryRepository;
 import org.springframework.stereotype.Component;
 
@@ -15,13 +13,13 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
-public class CreditEntryRepositoryImpl implements CreditHistoryRepository {
+public class CreditHistoryRepositoryImpl implements CreditHistoryRepository {
 
     private final CreditHistoryJpaRepository creditHistoryJpaRepository;
     private final CreditHistoryDataAccessMapper creditHistoryDataAccessMapper;
 
-    public CreditEntryRepositoryImpl(CreditHistoryJpaRepository creditHistoryJpaRepository,
-                                     CreditHistoryDataAccessMapper creditHistoryDataAccessMapper) {
+    public CreditHistoryRepositoryImpl(CreditHistoryJpaRepository creditHistoryJpaRepository,
+                                       CreditHistoryDataAccessMapper creditHistoryDataAccessMapper) {
         this.creditHistoryJpaRepository = creditHistoryJpaRepository;
         this.creditHistoryDataAccessMapper = creditHistoryDataAccessMapper;
     }
