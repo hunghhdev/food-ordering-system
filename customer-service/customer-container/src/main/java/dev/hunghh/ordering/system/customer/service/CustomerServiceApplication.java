4c152b4759522fc069cb8844f2119e22e1043a3e
hunghh.dev <hunghh.dev@gmail.com>
2023-01-30 21:52:24 +0700

    Creating Customer Kafka topic & Customer modules

--- customer-service/customer-container/src/main/java/dev/hunghh/ordering/system/customer/service/CustomerServiceApplication.java
+++ customer-service/customer-container/src/main/java/dev/hunghh/ordering/system/customer/service/CustomerServiceApplication.java
@@ -2,7 +2,11 @@ package dev.hunghh.ordering.system.customer.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = {"dev.hunghh.ordering.system.customer.service.dataaccess", "dev.hunghh.ordering.system.dataaccess"})
+@EntityScan(basePackages = {"dev.hunghh.ordering.system.customer.service.dataaccess", "dev.hunghh.ordering.system.dataaccess"})
 @SpringBootApplication(scanBasePackages = "dev.hunghh.ordering.system")
 public class CustomerServiceApplication {
 
